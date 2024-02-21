ea160f820851c80edd79f9c3d992f07815114f3b
conradMare <conradmare4@gmail.com>
2024-02-21 08:36:24 +0200

    2024/02/21(am)
    Lesson13_IOFundamentals

---
+++ Lesson13_IOFundamentals/L13_IOFundamentals/src/examples/PropertiesExample/com/example/TestProperties.java
@@ -0,0 +1,11 @@
+package examples.PropertiesExample.com.example;
+
+import java.util.Properties;
+
+public class TestProperties {
+
+    public static void main(String[] args) {
+        Properties props = System.getProperties();
+        props.list(System.out);
+    }
+}
---
+++ Lesson13_IOFundamentals/L13_IOFundamentals/src/examples/SerializeStockExample/com/example/StockException.java
@@ -0,0 +1,9 @@
+package examples.SerializeStockExample.com.example;
+
+public class StockException extends Exception {
+    private static final long serialVersionUID = 103L;
+
+    public StockException(String reason) {
+        super(reason);
+    }
+}
